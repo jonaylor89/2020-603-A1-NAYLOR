d47c6cd3a7cf9fe8aaabaecca72212538bc1c7c9
John Naylor <jonaylor89@gmail.com>
2020-09-12 14:22:40 -0400

    was this a bug?

--- main.cpp
+++ main.cpp
@@ -105,7 +105,7 @@ int* MPI_KNN(ArffData* dataset)
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     int* predictions = (int*)malloc(dataset->num_instances() * sizeof(int));
-    int portion = ceil(dataset->num_instances() / (size - 2));
+    int portion = ceil(dataset->num_instances() / (size - 1));
     if(rank == 0)
     {
 
