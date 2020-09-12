512747ea9cd5874acc5c02ac7728ddc7f15bda78
John Naylor <jonaylor89@gmail.com>
2020-09-12 14:31:58 -0400

    lemme play around real quick

--- main.cpp
+++ main.cpp
@@ -105,7 +105,8 @@ int* MPI_KNN(ArffData* dataset)
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     int* predictions = (int*)malloc(dataset->num_instances() * sizeof(int));
-    int portion = ceil(dataset->num_instances() / (size - 1));
+    // int portion = ceil(dataset->num_instances() / (size - 1));
+    int portion = 38;
     if(rank == 0)
     {
 
